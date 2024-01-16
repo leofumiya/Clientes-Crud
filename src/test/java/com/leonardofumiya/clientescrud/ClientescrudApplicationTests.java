3c73ee17687842383769ce589dd70a29290252b5
leofumiya <leonardo.fumiya@gmail.com>
2024-01-15 22:47:06 -0300

    Projeto criado

---
+++ src/test/java/com/leonardofumiya/clientescrud/ClientescrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonardofumiya.clientescrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientescrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
