522f07f6982300b8b200176f7b194dfed9a0f80c
leofumiya <leonardo.fumiya@gmail.com>
2024-01-18 21:05:50 -0300

    Começando a implementar o cadastro de endereços dos clientes

---
+++ src/main/java/com/leonardofumiya/clientescrud/dto/EnderecoDTO.java
@@ -0,0 +1,40 @@
+package com.leonardofumiya.clientescrud.dto;
+
+import com.leonardofumiya.clientescrud.entities.Endereco;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EnderecoDTO {
+
+    private Long id;
+    @NotBlank
+    private String logradouro;
+    @NotBlank
+    private String bairro;
+    @NotBlank
+    private String numero;
+    @NotBlank
+    @Pattern(regexp = "\\d{8}")
+    private String cep;
+    @NotBlank
+    private String cidade;
+    @NotBlank
+    private String uf;
+
+    public EnderecoDTO(Endereco entity) {
+        this.id = entity.getId();
+        this.logradouro = entity.getLogradouro();
+        this.bairro = entity.getBairro();
+        this.numero = entity.getNumero();
+        this.cep = entity.getCep();
+        this.cidade = entity.getCidade();
+        this.uf = entity.getUf();
+    }
+
+}
---
+++ src/main/java/com/leonardofumiya/clientescrud/entities/Endereco.java
@@ -0,0 +1,43 @@
+package com.leonardofumiya.clientescrud.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Table(name = "endereco")
+@Entity(name = "Endereco")
+@Getter
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Endereco {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String logradouro;
+    private String bairro;
+    private String numero;
+    private String cep;
+    private String cidade;
+    private String uf;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "endereco")
+    private List<Cliente> clientes = new ArrayList<>();
+
+    public Endereco(Long id, String logradouro, String bairro, String numero, String cep, String cidade, String uf) {
+        this.id = id;
+        this.logradouro = logradouro;
+        this.bairro = bairro;
+        this.numero = numero;
+        this.cep = cep;
+        this.cidade = cidade;
+        this.uf = uf;
+    }
+
+}
